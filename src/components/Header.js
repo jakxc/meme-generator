bf326878cbb3c9d25b296bd2490a906606052f86
jakxc <jack.x.chen@outlook.com>
2023-02-08 11:38:52 +1000

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header()
+{
+    return (
+        <header className="header">
+            <img src="./images/yaoming.jpg" className="header-image"></img>
+            <h2 className="header-title">Meme Generator</h2>
+            <h4 className="header-project">React Project - 2</h4>
+        </header>
+        
+    )
+}
+
+export default Header
